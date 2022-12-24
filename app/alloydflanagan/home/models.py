c6cc73ffbad3c95bf9a5b726c3c099f97383d8ac
A. Lloyd Flanagan <lloyd.flanagan@mobelux.com>
2022-12-24 08:26:42 -0600

    add intro field

--- app/alloydflanagan/home/models.py
+++ app/alloydflanagan/home/models.py
@@ -1,6 +1,11 @@
-from django.db import models
+from wagtail.admin.panels import FieldPanel
 from wagtail.core.models import Page
+from wagtail.fields import RichTextField
 
 
 class HomePage(Page):
-    pass
+    intro = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro", classname="full"),
+    ]
