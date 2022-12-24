344d8243a312419ef1a60ca2096a7a955f626898
A. Lloyd Flanagan <lloyd.flanagan@mobelux.com>
2022-12-24 08:25:55 -0600

    black reformat

--- app/alloydflanagan/blog/models.py
+++ app/alloydflanagan/blog/models.py
@@ -6,22 +6,22 @@ from wagtail.core.models import Page
 
 
 class BlogIndexPage(Page):
-    parent_page_types = ['home.HomePage']
+    parent_page_types = ["home.HomePage"]
 
 
 class BlogPost(Page):
-    parent_page_types = ['blog.BlogIndexPage']
+    parent_page_types = ["blog.BlogIndexPage"]
     posted_date = models.DateField(
         verbose_name=_("posted date"),
         null=True,
         blank=True,
         help_text='"Official" date post was written',
     )
-    primary_image = models.ImageField(null=True, upload_to='blog/images/')
+    primary_image = models.ImageField(null=True, upload_to="blog/images/")
     content = RichTextField()
 
     content_panels = Page.content_panels + [
-        FieldPanel('posted_date'),
-        FieldPanel('content'),
-        FieldPanel('primary_image')
+        FieldPanel("posted_date"),
+        FieldPanel("content"),
+        FieldPanel("primary_image"),
     ]
