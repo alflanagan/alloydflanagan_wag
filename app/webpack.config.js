28ee007e8567814af0e5f4d531c42ffcd6469d21
A. Lloyd Flanagan <lloyd.flanagan@mobelux.com>
2022-12-27 11:18:46 -0600

    add css plugin, standard config

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,14 +1,21 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './alloydflanagan/client/index.js',
+  plugins: [new MiniCssExtractPlugin()],
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'alloydflanagan/static')
+    // should == django.conf.settings.STATIC_ROOT
+    path: path.resolve(__dirname, 'static')
   },
   mode: 'development',
   module: {
     rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
       {
         test: /\.less$/i,
         use: [
