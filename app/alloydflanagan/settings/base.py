bb9f36b02f9e1da45cb8450b52554daf7fd453e6
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2023-10-01 16:48:39 -0400

    fresh launch and deploy to fly.io

--- app/alloydflanagan/settings/base.py
+++ app/alloydflanagan/settings/base.py
@@ -148,4 +148,4 @@ WAGTAILADMIN_BASE_URL = "admin/"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = "http://example.com"
+BASE_URL = "https://alloydflanagan.com"
