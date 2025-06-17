57b8d4d097f68df64815354bf16e735646dae94d
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-06-17 16:41:56 -0400

    Add FileField called resume to Home page model.

--- app/alloydflanagan/home/models.py
+++ app/alloydflanagan/home/models.py
@@ -1,11 +1,15 @@
+from django.db.models import FileField
+
 from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 
-
 class HomePage(Page):
     intro = RichTextField()
+    # may move resume to AboutPage model if/when we have one
+    resume = FileField(blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel("intro", classname="full"),
+        FieldPanel("resume", classname="full"),
     ]
