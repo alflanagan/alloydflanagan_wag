72efc15a9e03e32b4cbf08c94157831568dbc0db
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-06-21 13:59:06 -0400

    Add blog features (stolen from wagtail tutorial).

--- app/alloydflanagan/blog/models.py
+++ app/alloydflanagan/blog/models.py
@@ -5,24 +5,37 @@ from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField
 from wagtail.models import Page
 
+from wagtail.search import index
 
 class BlogIndexPage(Page):
-    parent_page_types = ["home.HomePage"]
+    intro = RichTextField(blank=True)
 
+    content_panels = Page.content_panels + [
+        FieldPanel("intro"),
+    ]
 
 class BlogPost(Page):
     parent_page_types = ["blog.BlogIndexPage"]
     posted_date = models.DateField(
-        verbose_name=_("posted date"),
+        verbose_name=_("Post Date"),
         null=True,
         blank=True,
         help_text='"Official" date post was written',
     )
     primary_image = models.ImageField(null=True, upload_to="blog/images/")
     content = RichTextField()
+    intro = models.CharField(max_length=250, default="")
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
 
     content_panels = Page.content_panels + [
         FieldPanel("posted_date"),
         FieldPanel("content"),
         FieldPanel("primary_image"),
+        FieldPanel("body"),
+        FieldPanel("intro"),
     ]
