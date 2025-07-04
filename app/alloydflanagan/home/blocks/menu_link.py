c0725f807591397ca0e759a1d8fa5ee9ab40d803
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-07-04 12:44:50 -0400

    Revert "Add content streamfield and blocks (#8)"
    This reverts commit b2c5b5c6593631d85b80b435db64c8de4e162865.
    Never use squash merge, what a mess.

--- app/alloydflanagan/home/blocks/menu_link.py
+++
@@ -1,9 +0,0 @@
-from wagtail.blocks import StructBlock, CharBlock, PageChooserBlock
-
-
-class MenuLinkBlock(StructBlock):
-    title = CharBlock(required=False)
-    target = PageChooserBlock(required=False)
-
-    class Meta:
-        template = 'home/templates/home/blocks/menu_link.html'
--- app/alloydflanagan/home/models.py
+++ app/alloydflanagan/home/models.py
@@ -1,26 +1,15 @@
 from django.db.models import FileField
 
 from wagtail.admin.panels import FieldPanel
-from wagtail.blocks import RichTextBlock
-from wagtail.images.blocks import ImageChooserBlock
 from wagtail.models import Page
-from wagtail.fields import RichTextField, StreamField
-
-from alloydflanagan.home.blocks.header import HeaderBlock
-
+from wagtail.fields import RichTextField
 
 class HomePage(Page):
     intro = RichTextField()
     # may move resume to AboutPage model if/when we have one
     resume = FileField(blank=True)
-    content = StreamField([
-        ('text', RichTextBlock()),  #probably will use custom type later
-        ('image', ImageChooserBlock()),
-        ('header', HeaderBlock()),
-    ], use_json_field=True, blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel("intro", classname="full"),
         FieldPanel("resume", classname="full"),
-        FieldPanel("content", classname="full"),
     ]
