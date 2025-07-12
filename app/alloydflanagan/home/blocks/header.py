e09680f598d0893b9a76585d8ce3c67374e15935
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-07-12 15:08:53 -0400

    Change HeaderBlock to be child of StructBlock not StreamBlock.

--- app/alloydflanagan/home/blocks/header.py
+++ app/alloydflanagan/home/blocks/header.py
@@ -1,9 +1,9 @@
-from wagtail.blocks import StreamBlock, ListBlock
+from wagtail.blocks import StructBlock, ListBlock
 from wagtail.images.blocks import ImageChooserBlock
 from alloydflanagan.home.blocks.menu_link import MenuLinkBlock
 
 
-class HeaderBlock(StreamBlock):
+class HeaderBlock(StructBlock):
     tabs = ListBlock(MenuLinkBlock())
     banner = ImageChooserBlock(required=False)
     class Meta:
