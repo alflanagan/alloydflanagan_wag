6f99f0b11710baee4e51240065fa44920a364d27
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-11-01 14:23:12 -0400

    Fix path to index.js entry point.

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './alloydflanagan/static/src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
