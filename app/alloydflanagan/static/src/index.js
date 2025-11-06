b2bf48fd1030018c6ca9c1a2467e03651e5c19b5
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-11-06 12:02:29 -0500

    CSS: add fallback fonts, set color for <a>.

--- app/alloydflanagan/static/src/index.js
+++ app/alloydflanagan/static/src/index.js
@@ -2,9 +2,8 @@
 // so they can all be registered here
 import { MenuLink } from './menu_link.js'
 import { PageHeader } from './page_header.js'
-/* eslint no-unused-vars: 0 */
-import css from '../css/index.css' // tell webpack to include file
-/* eslint no-unused-vars: 1 */
+// tell webpack to include styles
+import css from '../css/index.css' // eslint-disable-line
 
 /* global customElements */
 
