ed778f521006b5fa1d1e826f486f3f67e3fe3a19
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-11-22 13:50:49 -0500

    Implement basic nav for header, fix bugs.

--- app/alloydflanagan/static/src/page_header.js
+++ app/alloydflanagan/static/src/page_header.js
@@ -4,71 +4,83 @@ import { html, css, LitElement } from 'lit'
 export class PageHeader extends LitElement {
   static styles = css`
   header {
-  margin-bottom: 2rem;
-  margin-right: 15%;
-  margin-left: 15%;
-}
+    margin-bottom: 2rem;
+    margin-right: 15%;
+    margin-left: 15%;
+  }
 
-h3 {
-  margin-bottom: 0;
-  color: black;
-  margin-top: 2rem;
-}
+  h3 {
+    margin-bottom: 0;
+    color: black;
+    margin-top: 2rem;
+  }
 
-header h3 {
-  font-size: 3.5rem;
-  font-family: "OoohBaby", Helvetica, Arial, sans-serif;
-  font-style: cursive;
-  margin-top: 5rem;
-}
+  header h3 {
+    font-size: 3.5rem;
+    font-family: "Oooh Baby", Helvetica, Arial, sans-serif;
+    font-style: oblique;
+    margin-top: 5rem;
+  }
 
-nav {
-  margin-top: 1rem;
-}
+  nav {
+    margin-top: 1rem;
+  }
 
-nav menu-link {
-  padding: 0.25rem 0;
-  font-family: "Oooh Baby", serif;
-  font-weight: 700;
+  nav menu-link {
+    padding: 0.25rem 0;
+    font-family: "Oooh Baby", serif;
+    font-weight: 700;
 
-  /* font-variant: small-caps; */
-  color: rgb(255 255 255 / 100%);
-  background-color: transparent;
+    /* font-variant: small-caps; */
+    color: rgb(255 255 255 / 100%);
+    background-color: transparent;
 
-  /* border-bottom: 0.25rem solid transparent; */
-  font-size: 2.5rem;
-}
+    /* border-bottom: 0.25rem solid transparent; */
+    font-size: 2.5rem;
+  }
 
-nav menu-link:hover,
-nav menu-link:focus {
-  border-bottom-color: rgba(255 255 255 / 25%);
-}
+  nav menu-link:hover,
+  nav menu-link:focus {
+    border-bottom-color: rgba(255 255 255 / 25%);
+  }
 
-menu-link + menu-link {
-  margin-left: 1rem;
-}
+  menu-link + menu-link {
+    margin-left: 1rem;
+  }
 
-nav span {
-  margin-left: 15px;
-  margin-right: 15px;
-}
+  nav span {
+    margin-left: 15px;
+    margin-right: 15px;
+  }
 
-nav span:nth-child(1) {
-  margin-left: 0;
-}
+  nav span:nth-child(1) {
+    margin-left: 0;
+  }
 
-img {
-  max-height: 100px;
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: 100%;
-}
+  img {
+    max-height: 100px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 100%;
+  }
 `
 
   static properties = {
     banner: { type: String },
-    tabs: { type: String } // comma-separated
+    tabs: { type: String }, // comma-separated
+    urls: { type: String }, // comma-separated
+  }
+
+  /** Returns a single array whose elements are 2-tuples of successive elements of a and b.
+      The resulting array will have the length of the shorter of a or b. */
+  zip (a, b) {
+    const minN = Math.min(a.length, b.length)
+    const result = []
+    for (let i = 0; i < minN; i++) {
+      result.push([a[i], b[i]])
+    }
+    return result
   }
 
   render () {
@@ -76,7 +88,11 @@ img {
     if (this.banner) {
       banner = html`<img src="${this.banner}" alt="basic web site banner">`
     }
+
     const tabsList = this.tabs.split(',')
+    const urlList = this.urls.split(',')
+
+    const linkList = this.zip(tabsList, urlList)
 
     // note: final <div></div> seems to fix problem where content after page-header
     // became a child node, not a sibling. Not sure if a bug in Lit or something I did.
@@ -90,7 +106,7 @@ img {
             ${banner}
           </div>
           <nav class="nav nav-masthead justify-content-center app-nav">
-            ${tabsList.map(tab => html`<menu-link title="${tab}" />`)}
+            ${linkList.map(tab => html`<menu-link title="${tab[0]}" href="${tab[1]}" />`)}
           </nav>
         </div>
       </header>
