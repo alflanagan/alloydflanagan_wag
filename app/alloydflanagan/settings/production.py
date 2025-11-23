3c3676304d62b96cf814c32729524b5a437fb8fc
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-11-23 12:21:17 -0500

    Set up ALLOWED_HOSTS in production.

--- app/alloydflanagan/settings/production.py
+++ app/alloydflanagan/settings/production.py
@@ -8,3 +8,9 @@ except ImportError:
     pass
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+allowed_hosts = os.environ.get('DJANGO_ALLOWED_HOSTS')
+if allowed_hosts is None:
+    ALLOWED_HOSTS = ['*']
+else:
+    ALLOWED_HOSTS = allowed_hosts.split(',')
