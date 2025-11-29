3e0bbb767f3f07f64ac80659364e4efd6fcc50d3
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-11-29 17:19:47 -0500

    Remove wagtail header block.

--- app/alloydflanagan/home/blocks/header.py
+++
@@ -1,7 +0,0 @@
-from wagtail.blocks import StructBlock, ListBlock
-from wagtail.images.blocks import ImageChooserBlock
-
-class HeaderBlock(StructBlock):
-    banner = ImageChooserBlock(required=False)
-    class Meta:
-        template = 'home/blocks/header.html'
