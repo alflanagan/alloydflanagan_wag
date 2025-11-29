a626ad14fd8488d8b9b62e6c73b72f482ec14afd
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-11-29 17:30:00 -0500

    Remove wagtail from models.

--- app/alloydflanagan/blog/models.py
+++ app/alloydflanagan/blog/models.py
@@ -1,21 +1,10 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from wagtail.admin.panels import FieldPanel
-from wagtail.fields import RichTextField
-from wagtail.models import Page
+class BlogIndexPage(models.Model):
+    intro = models.TextField()
 
-from wagtail.search import index
-
-class BlogIndexPage(Page):
-    intro = RichTextField(blank=True, default='')
-
-    content_panels = Page.content_panels + [
-        FieldPanel("intro"),
-    ]
-
-class BlogPost(Page):
-    parent_page_types = ["blog.BlogIndexPage"]
+class BlogPost(models.Model):
     posted_date = models.DateField(
         verbose_name=_("Post Date"),
         null=True,
@@ -23,19 +12,6 @@ class BlogPost(Page):
         help_text='"Official" date post was written',
     )
     primary_image = models.ImageField(null=True, upload_to="blog/images/")
-    content = RichTextField()
+    content = models.TextField()
     intro = models.CharField(max_length=250, default="")
-    body = RichTextField(blank=True)
-
-    search_fields = Page.search_fields + [
-        index.SearchField('intro'),
-        index.SearchField('body'),
-    ]
-
-    content_panels = Page.content_panels + [
-        FieldPanel("posted_date"),
-        FieldPanel("content"),
-        FieldPanel("primary_image"),
-        FieldPanel("body"),
-        FieldPanel("intro"),
-    ]
+    body = models.TextField()
