03cea32f80fe773ce3be2a2b807b4f9dc8269a89
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-11-29 17:30:00 -0500

    Remove wagtail from models.

--- app/alloydflanagan/home/models.py
+++ app/alloydflanagan/home/models.py
@@ -1,24 +1,12 @@
-from wagtail.admin.panels import FieldPanel
-from wagtail.blocks import RichTextBlock
-from wagtail.images.blocks import ImageChooserBlock
-from wagtail.models import Page
-from wagtail.fields import RichTextField, StreamField
+from django.db import models
 
-from alloydflanagan.home.blocks.header import HeaderBlock
+class HomePage(models.Model):
+    intro = models.TextField()
+    # header = StreamField([
+    #     ('header', HeaderBlock())
+    # ], blank=True)
+    # content = StreamField([
+    #     ('text', RichTextBlock()),  #probably will use custom type later
+    #     ('image', ImageChooserBlock()),
+    # ], blank=True)
 
-
-class HomePage(Page):
-    intro = RichTextField()
-    header = StreamField([
-        ('header', HeaderBlock())
-    ], blank=True)
-    content = StreamField([
-        ('text', RichTextBlock()),  #probably will use custom type later
-        ('image', ImageChooserBlock()),
-    ], blank=True)
-
-    content_panels = Page.content_panels + [
-        FieldPanel('header', classname="full"),
-        FieldPanel("intro", classname="full"),
-        FieldPanel("content", classname="full"),
-    ]
