78346609fa98098d6ebc411e411e2809c20c5955
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-11-29 17:54:18 -0500

    Correct setup for /home, /admin URLs.

--- app/alloydflanagan/urls.py
+++ app/alloydflanagan/urls.py
@@ -2,14 +2,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
-from alloydflanagan.home import urls as home_urls
-from alloydflanagan.search import views as search_views
-
 urlpatterns = [
-    path("home/", home_urls, name="home"),
+    path("home/", include("alloydflanagan.home.urls"), name="home"),
     # path("blog/", blog.urls, name="blog"),
-    path("admin/", admin.site.urls),
-    path("search/", search_views.search, name="search"),
+    path("admin/", admin.site.urls, name="admin"),
 ]
 
 
