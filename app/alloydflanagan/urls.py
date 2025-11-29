8bc389c5aea50aa027e4ed77b3c7219576090c75
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-11-29 17:16:09 -0500

    Remove wagtail routes.

--- app/alloydflanagan/urls.py
+++ app/alloydflanagan/urls.py
@@ -2,17 +2,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
-from wagtail.admin import urls as wagtailadmin_urls
-from wagtail import urls as wagtail_urls
-from wagtail.documents import urls as wagtaildocs_urls
-
-from .home import urls as home_urls
-from .search import views as search_views
+from alloydflanagan.home import urls as home_urls
+from alloydflanagan.search import views as search_views
 
 urlpatterns = [
-    path("django-admin/", admin.site.urls),
-    path("admin/", include(wagtailadmin_urls)),
-    path("documents/", include(wagtaildocs_urls)),
+    path("home/", home_urls, name="home"),
+    # path("blog/", blog.urls, name="blog"),
+    path("admin/", admin.site.urls),
     path("search/", search_views.search, name="search"),
 ]
 
@@ -26,10 +22,3 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
-    path("", include(wagtail_urls)),
-]
