6d3ba605ae95d06e4ac722e364e8dd7c302f1665
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-12-23 15:57:51 -0600

    Remove unnecessary import.

--- app/alloydflanagan/static/src/index.js
+++ app/alloydflanagan/static/src/index.js
@@ -5,8 +5,6 @@
 import { MenuLink } from './menu_link.js'
 import { PageHeader } from './page_header.js'
 import { HeroBlock } from './hero.js'
-// tell webpack to include styles
-import css from '../css/index.css' // eslint-disable-line
 
 customElements.define('page-header', PageHeader)
 customElements.define('menu-link', MenuLink)
