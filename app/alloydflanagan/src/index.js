1ad535e3f382e7074e35fa4aee2a36aab4edb955
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2026-01-23 09:46:46 -0500

    Add basic 'principles' section to home page.

--- app/alloydflanagan/src/index.js
+++ app/alloydflanagan/src/index.js
@@ -1,13 +1,14 @@
-/* global customElements */
-
 // need to import components even if we don't use them directly
 // so they can all be registered here
 import { MenuLink } from './menu_link.js'
 import { PageHeader } from './page_header.js'
 import { HeroBlock } from './hero.js'
 import { AboutSection } from '../about_me/src/about_section'
+import { PrinciplesBlock} from './principles'
 
-customElements.define('page-header', PageHeader)
-customElements.define('menu-link', MenuLink)
-customElements.define('hero-block', HeroBlock)
-customElements.define('about-section', AboutSection)
+window.customElements.define('page-header', PageHeader)
+window.customElements.define('menu-link', MenuLink)
+window.customElements.define('hero-block', HeroBlock)
+window.customElements.define('about-section', AboutSection)
+window.customElements.define('principles-block', PrinciplesBlock)
+
