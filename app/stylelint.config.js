76e001fa5a4e4878396c4324b74b8e5366943c48
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2026-01-26 12:11:21 -0500

    Add plugins, no-unresolved-module rule.

--- app/stylelint.config.js
+++ app/stylelint.config.js
@@ -1,9 +1,16 @@
 /** @type {import('stylelint').Config} */
 export default {
   rules: {
-    'block-no-empty': true
+    'block-no-empty': true,
+    'plugin/no-unresolved-module': {
+      alias: {
+        assets: 'pebbles'
+      },
+      modules: ['node_modules', 'local_modules']
+    }
   },
   extends: ['stylelint-config-standard'],
+  plugins: ['stylelint-no-unresolved-module', "stylelint-declaration-strict-value"],
   overrides: [
     {
       files: ['**/*.{js,jsx,ts,tsx}'],
