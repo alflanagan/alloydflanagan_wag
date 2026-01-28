8a2055c3aeb166021dffd69f09b55313c4a7d2e6
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2026-01-28 18:29:49 -0500

    Increase size of title, center title and contents.

--- app/alloydflanagan/src/hero.js
+++ app/alloydflanagan/src/hero.js
@@ -3,24 +3,27 @@ import { html, css, LitElement } from 'lit'
 export class HeroBlock extends LitElement {
   static styles = css`
 
-  @import("../static/css/variables.css");
+@import("../static/css/variables.css");
 
-  .hero-block {
-    display: block;
-    border-color: var(--color-border);
-    border-width: var(--borde-width);
-    border-style: solid;
-    padding: var(--space-4);
-  }
+.hero-block {
+  display: block;
+  padding: var(--space-4);
+}
 
-  .title {
-    font-size: var(--text-xl);
-    color: var(--color-text-primary);
-  }
+.title {
+  font-size: var(--text-2xl);
+  color: var(--color-text-semibold);
+  margin-right: var(--space-auto);
+  margin-left: var(--space-auto);
+  width: fit-content;
+}
 
-  .content {
-    color: var(--color-text-muted);
-  }
+.content {
+  color: var(--color-text-primary);
+  margin-right: var(--space-auto);
+  margin-left: var(--space-auto);
+  width: fit-content;
+}
 `
 
   static properties = {
