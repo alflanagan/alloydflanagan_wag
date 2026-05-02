479d4b538889a4564c7459ee190ea1a40c7bfa7a
A. Lloyd Flanagan (he/him) <lloyd.flanagan@proton.me>
2026-05-02 17:32:26 -0400

    Prune the .venv uv install directory.

--- find.sh
+++ find.sh
@@ -3,6 +3,7 @@
 exec find . \
 	-name .terraform -prune -o \
 	-name .idea -prune -o \
+	-name .venv -prune -o \
 	-name '*cache*' -prune -o \
 	-name __pycache__ -prune -o \
 	-name .yarn -prune -o \
